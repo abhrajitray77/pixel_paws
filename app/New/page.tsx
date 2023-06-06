92ce2f1c31f349e5069778fc798e450b27cfae0b
abhrajitray77 <abhrajitray77@gmail.com>
2023-06-06 08:31:37 +0530

    Wishlist page works

--- app/New/page.tsx
+++ app/New/page.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+'use client'
+import Grid from '@/components/Grid';
+import { Game } from '@/gameTypes';
+import { gameList } from '@/rawg';
+import getPrice from '@/rawg/getPrice';
+import React, { useEffect, useState } from 'react'
 
 const NewR = () => {
+  const [games, setGames] = useState<Game[] | null>(null);
+  const [loading, setLoading] = useState(true);  //TO DO: add loading animation react spinner
+
+  //function to load games
+  useEffect(() => {
+    const loadGames = async () => {
+      const response = await gameList({ page_size: 50 });
+      let { results } = response;
+      results = results.filter((game) => game.ratings_count > 10);
+      results.forEach((game) => (game.price = getPrice(game)));
+      return results;
+    };
+    //setting games to the results of loadGames
+    (async () => {
+      try {
+        setGames(await loadGames());
+      } catch (error) {
+        console.error("Error loading games:", error);
+      }
+    })();
+  }, []);
   return (
-    <div className='text-white'>NewR</div>
+    <div>
+      <h1 className='text-gray-300 text-3xl font-bold'>New Releases</h1>
+      <div className="">
+      {games ? (
+        games.length ? (
+          <Grid games={games}/>
+        ) : (
+          <span className="NoGames">No games found.</span>
+        )
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+    </div>
   )
 }
 
